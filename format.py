019a1c5158a665c546887c4255e2ade63fc63c69
Thiago G. Q. <thiagogarianiq@gmail.com>
2025-03-24 21:35:46 -0300

    continue SZZ

--- format.py
+++ format.py
@@ -2,7 +2,6 @@ import json
 import os
 from utils.utils import indent_file, update_matched
 
-# indent_file(filename='./json/bics/bics_ss.json')
 for file in os.listdir('./json/bics'):
     indent_file(f"./json/bics/{file}")
-# indent_file("./json/raw_data/issues.json")
+    update_matched(f"./json/bics/{file}")
